7a0e40063fd33962c792a4286f63af367b081c3f
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-02-17 17:30:07 +0900

    main

--- packages/camera-app/src/App.tsx
+++ packages/camera-app/src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Camera App Powered by React</h1>
-      <Link to="/">Home</Link>
+      <a href="/">Home</a>
     </div>
   );
 }
